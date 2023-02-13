565dcc421823c7ac2ee3b3c5951032e755e9f486
Andy Hutton <shandy_hutton@hotmail.com>
2023-02-13 22:00:29 +0000

    started to add js

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 Project: skid-marks
 Developer: Andy Hutton
 Game tester: Theo Hutton
-A description of the project
-Im creating a came of top trumps that will allow my son to play along
+Im creating a came of top trumps that will allow my son to play along.
 
 1.0.0
+
+https://www.figma.com/file/wSQTWjVgF3N7chcYtWZcn8/Untitled?node-id=0%3A1&t=b1k4o6hQgyYHX4TQ-1
+wireframe example above including card
+
+examples below
+https://github.com/tuell/toptrumps/blob/master/js/index.js
+https://github.com/Wollivan/StarWarsTopTrumps/blob/main/script.js
---
+++ js/game.js
@@ -0,0 +1,34 @@
+class Car {
+  constructor(
+      name,
+      year,
+      bhp,
+      0-60,
+  ) 
+  
+  {
+      this.name = name;
+      this.year = year;
+      this.bhp = bhp;
+      this.0-60 = 0-60;
+}
+quattro = [Audi Quattro, 1985, 540,3.1]
+rs2000 = [Ford RS200, 1986, 580, 3.1]
+impreza = [Subaru Impreza 22B, 1997, 310, 5.0]
+evo = [Mitsubishi Evo IV, 1997, 280, 5.5]
+ffocus = [Ford FocusEvent, 1999, 300, 4.2]
+suzuki = [Suzuki Escudo Pikes Peak, 1996, 985, 2.5]
+mg = [MG Metro 6R4, 1985, 410, 3.0]
+nissan = [Nissan GT-R, 2023, 565, 2.9]
+porsche = [Porsche 911 GT3 RS, 2022, 523, 2.7 ]
+m3 = [BMW M3 GTS, 2016, 306, 4.4]
+ferrari = [Ferrari F40, 1987, 471, 3.0]
+mustang = [Ford Mustang,2020, 460, 4.2]
+lambo = [Lamborghini LP 610, 2018, 601, 3.2]
+mazda = [Mazda RX7, 1992, 197, 4.1]
+golf = [Volkswagen Golf R, 2020, 292, 4.9 ]
+renault = [Renault Meganne Trophy, 2016, 300, 5.7 ]
+honda = [Honda Civic Type R, 2021, 310, 4.8 ]
+tesla = [Tesla Model S, 2021, 310, 3.2 ]
+caterham = [Caterham 7, 2015, 180, 4.8]
+hyundai = [Hyundai I30n, 2020, 271, 5.9]
