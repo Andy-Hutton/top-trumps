f22115dbc439573d7e57540e25fc34b37f2d3f67
Andy Hutton <shandy_hutton@hotmail.com>
2023-02-14 11:06:17 +0000

    added vehicle datail in js

--- js/game.js
+++ js/game.js
@@ -1,34 +1,44 @@
-class Car {
-  constructor(
-      name,
-      year,
-      bhp,
-      0-60,
-  ) 
+"choose card"
+"you won"
+"you lost"
+"its a draw"
+"new game"
+"game over"
+
+let numberOfRounds = 5;
+let numberOfCategories = 4;
+
+
   
-  {
+  function SkidMarks(name, year, bhp, acel){
       this.name = name;
       this.year = year;
       this.bhp = bhp;
-      this.0-60 = 0-60;
+      this.acel = acel;
 }
-quattro = [Audi Quattro, 1985, 540,3.1]
-rs2000 = [Ford RS200, 1986, 580, 3.1]
-impreza = [Subaru Impreza 22B, 1997, 310, 5.0]
-evo = [Mitsubishi Evo IV, 1997, 280, 5.5]
-ffocus = [Ford FocusEvent, 1999, 300, 4.2]
-suzuki = [Suzuki Escudo Pikes Peak, 1996, 985, 2.5]
-mg = [MG Metro 6R4, 1985, 410, 3.0]
-nissan = [Nissan GT-R, 2023, 565, 2.9]
-porsche = [Porsche 911 GT3 RS, 2022, 523, 2.7 ]
-m3 = [BMW M3 GTS, 2016, 306, 4.4]
-ferrari = [Ferrari F40, 1987, 471, 3.0]
-mustang = [Ford Mustang,2020, 460, 4.2]
-lambo = [Lamborghini LP 610, 2018, 601, 3.2]
-mazda = [Mazda RX7, 1992, 197, 4.1]
-golf = [Volkswagen Golf R, 2020, 292, 4.9 ]
-renault = [Renault Meganne Trophy, 2016, 300, 5.7 ]
-honda = [Honda Civic Type R, 2021, 310, 4.8 ]
-tesla = [Tesla Model S, 2021, 310, 3.2 ]
-caterham = [Caterham 7, 2015, 180, 4.8]
-hyundai = [Hyundai I30n, 2020, 271, 5.9]
+
+
+const quattro = [Audi Quattro, 1985, 540,3.1]
+const rs2000 = [Ford RS200, 1986, 580, 3.1]
+const impreza = [Subaru Impreza 22B, 1997, 310, 5.0]
+const evo = [Mitsubishi Evo IV, 1997, 280, 5.5]
+const ffocus = [Ford Focus, 1999, 300, 4.2]
+const suzuki = [Suzuki Escudo Pikes Peak, 1996, 985, 2.5]
+const mg = [MG Metro 6R4, 1985, 410, 3.0]
+const nissan = [Nissan GT-R, 2023, 565, 2.9]
+const porsche = [Porsche 911 GT3 RS, 2022, 523, 2.7 ]
+const m3 = [BMW M3 GTS, 2016, 306, 4.4]
+const ferrari = [Ferrari F40, 1987, 471, 3.0]
+const mustang = [Ford Mustang,2020, 460, 4.2]
+const lambo = [Lamborghini LP 610, 2018, 601, 3.2]
+const mazda = [Mazda RX7, 1992, 197, 4.1]
+const golf = [Volkswagen Golf R, 2020, 292, 4.9 ]
+const renault = [Renault Meganne Trophy, 2016, 300, 5.7 ]
+const honda = [Honda Civic Type R, 2021, 310, 4.8 ]
+const tesla = [Tesla Model S, 2021, 310, 3.2 ]
+const caterham = [Caterham 7, 2015, 180, 4.8]
+const hyundai = [Hyundai I30n, 2020, 271, 5.9]
+const caterham = [Caterham 7, 2015, 180, 4.8]
+
+
+const allCards = [quattro, rs2000, impreza, evo, ffocus, suzuki, mg, nissan, porsche, m3, ferrari, mustang, lambo, mazda, golf, renault, honda, tesla, hyundai]
